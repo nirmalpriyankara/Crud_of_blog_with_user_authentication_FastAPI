bbe2131c5be94207413b32ef7ba319da457f786c
Nirmal Priyankara <132396660+nirmalpriyankara@users.noreply.github.com>
2024-05-18 01:48:57 +0530

    four basic functions

--- main.py
+++ main.py
@@ -5,8 +5,24 @@ app = FastAPI()
 @app.get('/')
 
 def index():
-   return {'data':{'name':'Nirmal'}}
+   return {'data':'Blog List'}
 
-@app.get('/about')
-def about():
-   return {'data':'about page'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+   return{'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+
+def show(id: int):
+#fetch blog with id=id
+   return {'data':id}
+
+
+
+@app.get('/blog/{id}/comments')
+
+def comments(id):
+#fetch comments of blog with id=id
+   return {'data':{'1','2'}}
